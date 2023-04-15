2b65cf710c5876eea818411fc9b6d31720b9a2b9
elenazmeeva <biertli@bk.ru>
2023-04-15 20:22:06 +0300

    домашнее задание

---
+++ src/transport/Car.java
@@ -0,0 +1,8 @@
+package transport;
+
+public class Car extends Transport{
+    public Car(String brand, String model, int year, String country, String color, int maxSpeed) {
+        super(brand, model, year, country, color, maxSpeed);
+    }
+
+}
